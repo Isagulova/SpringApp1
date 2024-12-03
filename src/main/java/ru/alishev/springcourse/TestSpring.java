b354654460e5bed8f00b56288854c3a184731df4
Isagulova <isagulovaburulsun05@gmail.com>
2024-12-03 15:55:44 +0600

    annotatoions part2

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -18,7 +18,9 @@ public class TestSpring {
 //        Music music2 = context.getBean("classicalMusic", Music.class);
 //        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
 //        classicalMusicPlayer.playMusic();
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }}
