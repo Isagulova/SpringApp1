2a20d305195a0f0c8a6b66da6f6b5967b1b3a6c9
Isagulova <isagulovaburulsun05@gmail.com>
2024-12-09 21:48:29 +0600

    was added @PostConstruct and @PreDestrop

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -1,8 +1,21 @@
 package ru.alishev.springcourse;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
 @Component
+@Scope("prototype")
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    @PreDestroy
+    public void doMyDestoy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
